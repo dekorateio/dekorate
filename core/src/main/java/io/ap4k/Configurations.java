e5117fa59453d193ea7fe3293934f6580c349716
Ioannis Canellos <iocanel@gmail.com>
2018-11-15 22:55:38 +0200

    feat: Introduce explicit groups (groups that can only be added resources/visitors if explicitly specified). Resources and Configurations are now ordered.

--- core/src/main/java/io/ap4k/Configurations.java
+++ core/src/main/java/io/ap4k/Configurations.java
@@ -5,13 +5,14 @@ import io.ap4k.config.ConfigurationSupplier;
 import io.fabric8.kubernetes.api.builder.Visitor;
 
 import java.util.HashSet;
+import java.util.LinkedHashSet;
 import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Configurations {
 
-  private final Set<ConfigurationSupplier<? extends Configuration>> suppliers = new HashSet<>();
+  private final Set<ConfigurationSupplier<? extends Configuration>> suppliers = new LinkedHashSet<>();
   private final Set<Visitor> visitors = new HashSet<>();
 
   public void add(ConfigurationSupplier supplier) {
--- core/src/main/java/io/ap4k/Resources.java
+++ core/src/main/java/io/ap4k/Resources.java
@@ -8,6 +8,8 @@ import io.fabric8.kubernetes.api.model.KubernetesListBuilder;
 
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.LinkedHashMap;
+import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -15,9 +17,11 @@ import java.util.stream.Collectors;
 
 public class Resources {
 
-  private final Map<String, KubernetesListBuilder> groups = new HashMap<>();
-  private final Set<KubernetesListBuilder> global = new HashSet<>();
+  private final Map<String, KubernetesListBuilder> groups = new LinkedHashMap<>();
+  private final Set<KubernetesListBuilder> global = new LinkedHashSet<>();
   private final Set<Visitor> visitors = new HashSet<>();
+
+  private final Map<String, KubernetesListBuilder> explicitGroups = new HashMap<>();
 
   /**
    * Add a {@link Visitor}.
@@ -33,10 +37,14 @@ public class Resources {
    * @param visitor   The visitor.
    */
   public void accept(String group, Visitor visitor) {
-    if (!groups.containsKey(group)) {
+    if (explicitGroups.containsKey(group)) {
+      explicitGroups.get(group).accept(visitor);
+
+    } else if (groups.containsKey(group)) {
+      groups.get(group).accept(visitor);
+    } else {
       throw new IllegalArgumentException("Cannot add visitor for non existent resource group:" + group);
     }
-    groups.get(group).accept(visitor);
   }
 
   /**
@@ -65,6 +73,19 @@ public class Resources {
       groups.put(group, new KubernetesListBuilder());
     }
     groups.get(group).addToItems(metadata);
+  }
+
+  /**
+   * Add a resource to the specified explicit group.
+   * Explicit groups only accept visitors explicitly assigned to them.
+   * @param group     The group.
+   * @param metadata  The resource.
+   */
+  public void addExplicit(String group, HasMetadata metadata) {
+    if (!explicitGroups.containsKey(group)) {
+      explicitGroups.put(group, new KubernetesListBuilder());
+    }
+    explicitGroups.get(group).addToItems(metadata);
   }
 
   /**
@@ -100,6 +121,7 @@ public class Resources {
 
     visitors.forEach(v -> groups.forEach((g, b) -> b.accept(v)));
     groups.forEach((g, b) -> resources.put(g, b.build()));
+    explicitGroups.forEach((g, b) -> resources.put(g, b.build()));
     return resources;
   }
 }
