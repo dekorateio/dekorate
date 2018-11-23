1b021c2a9a918f2ba2e9948c6d57ae043779b19b
Ioannis Canellos <iocanel@gmail.com>
2018-11-23 18:20:55 +0200

    fix: add missing dependency from component-service-example.

--- annotations/component-annotations/src/main/java/io/ap4k/component/visitor/AddServiceInstanceToComponent.java
+++ annotations/component-annotations/src/main/java/io/ap4k/component/visitor/AddServiceInstanceToComponent.java
@@ -4,6 +4,8 @@ import io.ap4k.component.model.ComponentSpecBuilder;
 import io.ap4k.servicecatalog.config.ServiceCatalogInstance;
 import io.ap4k.deps.kubernetes.api.builder.TypedVisitor;
 
+import java.util.Arrays;
+
 public class AddServiceInstanceToComponent extends TypedVisitor<ComponentSpecBuilder> {
 
   private final ServiceCatalogInstance instance;
@@ -14,11 +16,19 @@ public class AddServiceInstanceToComponent extends TypedVisitor<ComponentSpecBui
 
   @Override
   public void visit(ComponentSpecBuilder component) {
+    if (hasService(component)) {
+      return;
+    }
+
     component.addNewService()
       .withName(instance.getName())
       .withServiceClass(instance.getServiceClass())
       .withServicePlan(instance.getServicePlan())
       .withSecretName(instance.getBindingSecret())
       .endService();
+  }
+
+  private boolean hasService(ComponentSpecBuilder componentSpec) {
+    return Arrays.asList(componentSpec.getService()).stream().filter(s -> s.getName().equals(instance.getName())).count() > 0;
   }
 }
