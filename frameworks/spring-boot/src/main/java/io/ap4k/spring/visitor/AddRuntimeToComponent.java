3c4d4793c4118d2c6c285c3b86e16802be0ca439
Charles Moulliard <cmoulliard@redhat.com>
2018-11-23 11:58:20 +0100

    Add question TODO

--- frameworks/spring-boot/src/main/java/io/ap4k/spring/visitor/AddRuntimeToComponent.java
+++ frameworks/spring-boot/src/main/java/io/ap4k/spring/visitor/AddRuntimeToComponent.java
@@ -4,6 +4,7 @@ import io.ap4k.component.model.ComponentSpecBuilder;
 
 public class AddRuntimeToComponent extends TypedVisitor<ComponentSpecBuilder> {
 
+  // TODO - Should I decorate Component here ?
   @Override
   public void visit(ComponentSpecBuilder component) {
     component.withRuntime(RuntimeFrameworkDetector.detect());
