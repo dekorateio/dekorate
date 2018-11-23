f6d6633eebbb2a6f32f43184dea3626c11b15f80
Charles Moulliard <cmoulliard@redhat.com>
2018-11-23 15:34:16 +0100

    Add missing testing gavs

--- examples/component-service-example/src/test/java/io/ap4k/examples/ComponentServiceSpringBootExampleTest.java
+++ examples/component-service-example/src/test/java/io/ap4k/examples/ComponentServiceSpringBootExampleTest.java
@@ -1,14 +1,31 @@
+/**
+ * Copyright 2015 The original authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ **/
+
 package io.ap4k.examples;
 
 import io.ap4k.deps.kubernetes.api.model.HasMetadata;
 import io.ap4k.deps.kubernetes.api.model.KubernetesList;
 import io.ap4k.deps.servicecatalog.api.model.ServiceInstance;
 import io.ap4k.utils.Serialization;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
-import static junit.framework.TestCase.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.Assert.assertEquals;
 
 public class ComponentServiceSpringBootExampleTest {
