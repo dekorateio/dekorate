34a7165b4ccfea9d4b21e5984cad567bd617fec3
Charles Moulliard <cmoulliard@redhat.com>
2018-11-23 15:05:16 +0100

    Extend the component service test case

--- examples/component-service-example/src/test/java/io/ap4k/examples/ComponentServiceSpringBootExampleTest.java
+++ examples/component-service-example/src/test/java/io/ap4k/examples/ComponentServiceSpringBootExampleTest.java
@@ -1,8 +1,12 @@
 package io.ap4k.examples;
 
+import io.ap4k.deps.kubernetes.api.model.HasMetadata;
 import io.ap4k.deps.kubernetes.api.model.KubernetesList;
+import io.ap4k.deps.servicecatalog.api.model.ServiceInstance;
 import io.ap4k.utils.Serialization;
 import org.junit.Test;
+
+import java.util.List;
 
 import static junit.framework.TestCase.assertNotNull;
 import static org.junit.Assert.assertEquals;
@@ -10,9 +14,13 @@ import static org.junit.Assert.assertEquals;
 public class ComponentServiceSpringBootExampleTest {
 
   @Test
-  public void shouldContainDeploymentConfig() {
+  public void shouldContainComponentService() {
     KubernetesList list = Serialization.unmarshal(ComponentServiceSpringBootExampleTest.class.getClassLoader().getResourceAsStream("META-INF/ap4k/component.yml"));
     assertNotNull(list);
-    assertEquals(1, list.getItems().size());
+    List<HasMetadata> items = list.getItems();
+    assertEquals(1, items.size());
+    ServiceInstance svc = (ServiceInstance)items.get(0);
+    assertNotNull(svc);
+    assertEquals("mysql-instance",svc.getMetadata().getName());
   }
 }
