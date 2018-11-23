a9de180928b70a00323520b3f4a155a0d12be5bc
Charles Moulliard <cmoulliard@redhat.com>
2018-11-23 15:35:24 +0100

    Change to junit.jupiter class

--- examples/component-example/src/test/java/io/ap4k/examples/ComponentSpringBootExampleTest.java
+++ examples/component-example/src/test/java/io/ap4k/examples/ComponentSpringBootExampleTest.java
@@ -4,7 +4,7 @@ import io.ap4k.deps.kubernetes.api.model.KubernetesList;
 import io.ap4k.utils.Serialization;
 import org.junit.jupiter.api.Test;
 
-import static junit.framework.TestCase.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.Assert.assertEquals;
 
 public class ComponentSpringBootExampleTest {
