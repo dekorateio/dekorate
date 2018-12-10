bc776990cc6c8c847d05fdb68e68f8d262c8ceed
Ioannis Canellos <iocanel@gmail.com>
2018-12-10 13:06:00 +0200

    feat: add missing integration test.

---
+++ examples/spring-boot-on-openshift-example/src/test/java/io/ap4k/example/sbonopenshift/SpringBootOnOpenshiftIT.java
@@ -0,0 +1,50 @@
+package io.ap4k.example.sbonopenshift;
+
+import io.ap4k.deps.kubernetes.api.model.KubernetesList;
+import io.ap4k.deps.kubernetes.api.model.Pod;
+import io.ap4k.deps.kubernetes.client.KubernetesClient;
+import io.ap4k.deps.kubernetes.client.LocalPortForward;
+import io.ap4k.deps.okhttp3.OkHttpClient;
+import io.ap4k.deps.okhttp3.Request;
+import io.ap4k.deps.okhttp3.Response;
+import io.ap4k.testing.annotation.Inject;
+import io.ap4k.testing.annotation.Named;
+import io.ap4k.testing.openshift.annotation.OpenshiftIntegrationTest;
+import org.junit.jupiter.api.Test;
+
+import java.net.URL;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@OpenshiftIntegrationTest
+class SpringBootOnOpenshiftIT {
+  @Inject
+  private KubernetesClient client;
+
+  @Inject
+  private KubernetesList list;
+
+  @Inject
+  @Named("spring-boot-on-openshift-example")
+  Pod pod;
+
+  @Test
+  public void shouldRespondWithHelloWorld() throws Exception {
+    assertNotNull(client);
+    assertNotNull(list);
+    System.out.println("Forwarding port");
+    try (LocalPortForward p = client.pods().withName(pod.getMetadata().getName()).portForward(8080)) {
+      assertTrue(p.isAlive());
+      URL url = new URL("http://localhost:"+p.getLocalPort()+"/");
+
+      OkHttpClient client = new OkHttpClient();
+      Request request = new Request.Builder().get().url(url).build();
+      Response response = client.newCall(request).execute();
+      assertEquals(response.body().string(), "Hello world");
+    } catch (Exception e)  {
+      e.printStackTrace();
+    }
+  }
+
+}
