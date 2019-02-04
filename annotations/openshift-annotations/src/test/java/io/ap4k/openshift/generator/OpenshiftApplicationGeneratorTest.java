be349795a942dce8005a7022de1547e4652c9f8f
Ioannis Canellos <iocanel@gmail.com>
2019-02-04 14:13:19 +0200

    fix: broken test.

--- annotations/openshift-annotations/src/test/java/io/ap4k/openshift/generator/OpenshiftApplicationGeneratorTest.java
+++ annotations/openshift-annotations/src/test/java/io/ap4k/openshift/generator/OpenshiftApplicationGeneratorTest.java
@@ -3,19 +3,21 @@ package io.ap4k.openshift.generator;
 import io.ap4k.Session;
 import io.ap4k.SessionWriter;
 import io.ap4k.deps.kubernetes.api.model.KubernetesList;
-import io.ap4k.deps.kubernetes.api.model.apps.Deployment;
+import io.ap4k.deps.openshift.api.model.DeploymentConfig;
 import io.ap4k.openshift.annotation.OpenshiftApplication;
 import io.ap4k.processor.SimpleFileWriter;
+import io.ap4k.project.FileProjectFactory;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.HashMap;
 import java.util.Map;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.*;
 
 class OpenshiftApplicationGeneratorTest {
   static Path tempDir;
@@ -26,12 +28,14 @@ class OpenshiftApplicationGeneratorTest {
   }
 
   @Test
-  public void shouldGenerateKubernetes()  {
+  public void shouldGenerateOpenshift()  {
     SessionWriter writer = new SimpleFileWriter(tempDir);
     Session session = Session.getSession();
     session.setWriter(writer);
 
     OpenshiftApplicationGenerator generator = new OpenshiftApplicationGenerator() {};
+    generator.setProject(FileProjectFactory.create(new File(".")));
+    System.out.println("Project root:" + generator.getProject());
 
     Map<String, Object> map = new HashMap<String, Object>() {{
       put(OpenshiftApplication.class.getName(), new HashMap<String, Object>() {{
@@ -43,7 +47,7 @@ class OpenshiftApplicationGeneratorTest {
 
     generator.add(map);
     session.close();
-    KubernetesList list=session.getGeneratedResources().get("kubernetes");
+    KubernetesList list=session.getGeneratedResources().get("openshift");
     assertThat(list).isNotNull();
     assertThat(list.getItems())
       .filteredOn(i -> "DeploymentConfig".equals(i.getKind()))
