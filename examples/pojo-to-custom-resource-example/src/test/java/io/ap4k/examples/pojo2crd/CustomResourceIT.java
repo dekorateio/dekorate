58242a27a592cff553c40e0a6fb7dc9656a0eb35
Ioannis Canellos <iocanel@gmail.com>
2019-02-12 18:07:42 +0200

    fix: disable auto deploy in the custom resource it (due to client bugs / limitations).

--- examples/pojo-to-custom-resource-example/src/test/java/io/ap4k/examples/pojo2crd/CustomResourceIT.java
+++ examples/pojo-to-custom-resource-example/src/test/java/io/ap4k/examples/pojo2crd/CustomResourceIT.java
@@ -1,6 +1,7 @@
 package io.ap4k.examples.pojo2crd;
 
 import io.ap4k.testing.WithProject;
+import io.ap4k.testing.annotation.KubernetesIntegrationTest;
 import io.ap4k.utils.Exec;
 import org.junit.jupiter.api.Test;
 
@@ -8,6 +9,7 @@ import java.nio.file.Path;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+@KubernetesIntegrationTest(isAutoDeployEnabled = false)
 class CustomResourceIT implements WithProject {
 
   Path testResources = getProject().getRoot().resolve("src").resolve("test").resolve("resources");
@@ -15,7 +17,7 @@ class CustomResourceIT implements WithProject {
 
   @Test
   void shouldApplyFoo() {
-    assertTrue(Exec.inProject(getProject()).commands("oc", "create", "-f", crdPath.toAbsolutePath().toString()));
+    assertTrue(Exec.inProject(getProject()).commands("oc", "apply", "-f", crdPath.toAbsolutePath().toString()));
     assertTrue(Exec.inProject(getProject()).commands("oc", "create", "-f", testResources.resolve("test-foo.yml").toAbsolutePath().toString()));
     assertTrue(Exec.inProject(getProject()).commands("oc", "delete", "-f", testResources.resolve("test-foo.yml").toAbsolutePath().toString()));
     assertTrue(Exec.inProject(getProject()).commands("oc", "delete", "-f", crdPath.toAbsolutePath().toString()));
