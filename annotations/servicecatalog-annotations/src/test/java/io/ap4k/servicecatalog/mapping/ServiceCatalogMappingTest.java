c1844f11f39bc2180cd5096af1593fb98fceeb96
Ioannis Canellos <iocanel@gmail.com>
2019-02-17 23:32:31 +0200

    feat: add new example with service catalog on openshift that demonstrates binding.

---
+++ annotations/servicecatalog-annotations/src/test/java/io/ap4k/servicecatalog/mapping/ServiceCatalogMappingTest.java
@@ -0,0 +1,41 @@
+/**
+ * Copyright 2018 The original authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+**/
+package io.ap4k.servicecatalog.mapping;
+
+import io.ap4k.deps.kubernetes.api.KubernetesResourceMappingProvider;
+import io.ap4k.deps.kubernetes.api.model.KubernetesList;
+import io.ap4k.utils.Serialization;
+import org.junit.jupiter.api.Test;
+
+import java.util.ServiceLoader;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+public class ServiceCatalogMappingTest {
+
+  @Test
+  void shouldLoadProvider() {
+    ServiceLoader loader = ServiceLoader.load(KubernetesResourceMappingProvider.class);
+    loader.forEach(l -> System.out.println("Found loader:" + l ));
+  }
+
+  @Test
+  public void shouldContainDeployment() {
+    KubernetesList list = Serialization.unmarshal(ServiceCatalogMappingTest.class.getClassLoader().getResourceAsStream("svcat.yml"));
+    assertNotNull(list);
+  }
+}
