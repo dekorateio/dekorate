08d91e716cf859ee06739544f74ecb5c092ee23f
aurea munoz <amunozhe@redhat.com>
2019-05-22 12:48:46 +0200

    wip :  move Link annotation to standalone
    Related to SB-1095

--- annotations/component-annotations/src/main/java/io/ap4k/component/annotation/CompositeApplication.java
+++ annotations/component-annotations/src/main/java/io/ap4k/component/annotation/CompositeApplication.java
@@ -20,6 +20,7 @@ package io.ap4k.component.annotation;
 import io.ap4k.component.model.DeploymentMode;
 import io.ap4k.kubernetes.config.Configuration;
 import io.ap4k.kubernetes.annotation.Env;
+import io.ap4k.kubernetes.config.Configuration;
 import io.sundr.builder.annotations.Adapter;
 import io.sundr.builder.annotations.Buildable;
 import io.sundr.builder.annotations.Pojo;
