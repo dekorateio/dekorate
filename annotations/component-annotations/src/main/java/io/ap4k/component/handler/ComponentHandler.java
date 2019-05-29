e79b2387d9959e1bc6955aa731914603ca36a2f0
Chris Laprun <metacosm@gmail.com>
2019-05-29 17:45:37 +0200

    refactor: remove decorators to simplify

--- annotations/component-annotations/src/main/java/io/ap4k/component/handler/ComponentHandler.java
+++ annotations/component-annotations/src/main/java/io/ap4k/component/handler/ComponentHandler.java
@@ -1,30 +1,27 @@
 /**
  * Copyright 2018 The original authors.
- * 
+ * <p>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
- *     http://www.apache.org/licenses/LICENSE-2.0
- * 
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- * 
-**/
+ **/
 package io.ap4k.component.handler;
 
 import io.ap4k.Handler;
 import io.ap4k.Resources;
 import io.ap4k.component.config.CompositeConfig;
 import io.ap4k.component.config.EditableCompositeConfig;
-import io.ap4k.component.decorator.AddEnvToComponentDecorator;
-import io.ap4k.component.decorator.AddRuntimeTypeToComponentDecorator;
-import io.ap4k.component.decorator.AddRuntimeVersionToComponentDecorator;
 import io.ap4k.component.model.Component;
 import io.ap4k.component.model.ComponentBuilder;
+import io.ap4k.component.model.ComponentFluent;
 import io.ap4k.kubernetes.config.ConfigKey;
 import io.ap4k.kubernetes.config.Configuration;
 import io.ap4k.kubernetes.config.Env;
@@ -53,7 +50,6 @@ public class ComponentHandler implements Handler<CompositeConfig> {
       resources.setName(config.getName());
     }
     resources.addCustom(COMPONENT, createComponent(config));
-    addVisitors(config);
   }
 
   @Override
@@ -62,37 +58,38 @@ public class ComponentHandler implements Handler<CompositeConfig> {
       type.equals(EditableCompositeConfig.class);
   }
 
-  private void addVisitors(CompositeConfig config) {
-    String type = config.getAttribute(RUNTIME_TYPE);
-    String version = config.getAttribute(RUNTIME_VERSION);
-    
-    if (type != null) {
-      resources.decorateCustom(COMPONENT,new AddRuntimeTypeToComponentDecorator(type));
-    }
-
-    if (version != null) {
-      resources.decorateCustom(COMPONENT,new AddRuntimeVersionToComponentDecorator(version));
-    }
-    for (Env env : config.getEnvVars()) {
-      resources.decorateCustom(COMPONENT, new AddEnvToComponentDecorator(env));
-    }
-  }
-
   /**
    * Create a {@link Component} from a {@link CompositeConfig}.
    * @param config  The config.
-   * @return        The component.
+   * @return The component.
    */
   private Component createComponent(CompositeConfig config) {
-    return new ComponentBuilder()
+    final ComponentFluent.SpecNested<ComponentBuilder> specBuilder = new ComponentBuilder()
       .withNewMetadata()
       .withName(resources.getName())
       .withLabels(resources.getLabels())
       .endMetadata()
-      .withNewSpec()
+      .withNewSpec();
+
+    final String type = config.getAttribute(RUNTIME_TYPE);
+    if (type != null) {
+      specBuilder.withRuntime(type);
+    }
+
+    final String version = config.getAttribute(RUNTIME_VERSION);
+    if (version != null) {
+      specBuilder.withVersion(version);
+    } else {
+      specBuilder.withVersion(resources.getVersion());
+    }
+
+    for (Env env : config.getEnvVars()) {
+      specBuilder.addNewEnv(env.getName(), env.getValue());
+    }
+
+    return specBuilder
       .withDeploymentMode(config.getDeploymentMode())
       .withExposeService(config.isExposeService())
-      .withVersion(resources.getVersion())
       .endSpec()
       .build();
   }
