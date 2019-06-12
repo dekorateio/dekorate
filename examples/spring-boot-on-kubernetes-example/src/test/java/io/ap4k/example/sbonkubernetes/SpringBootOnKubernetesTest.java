dc3f3062f27285deb75680a03e2efc68deb4ea92
Chris Laprun <metacosm@gmail.com>
2019-06-12 17:06:19 +0200

    feat: prefix annotationless properties by `dekorate` prefix

--- examples/spring-boot-on-kubernetes-example/src/test/java/io/ap4k/example/sbonkubernetes/SpringBootOnKubernetesTest.java
+++ examples/spring-boot-on-kubernetes-example/src/test/java/io/ap4k/example/sbonkubernetes/SpringBootOnKubernetesTest.java
@@ -1,31 +1,29 @@
 /**
  * Copyright 2018 The original authors.
- * 
+ * <p>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
- *     http://www.apache.org/licenses/LICENSE-2.0
- * 
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- * 
-**/
+ **/
 package io.ap4k.example.sbonkubernetes;
 
 import io.ap4k.deps.kubernetes.api.model.HasMetadata;
 import io.ap4k.deps.kubernetes.api.model.KubernetesList;
-import io.ap4k.deps.kubernetes.api.model.Service;
 import io.ap4k.deps.kubernetes.api.model.apps.Deployment;
 import io.ap4k.utils.Serialization;
 import org.junit.jupiter.api.Test;
 
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 class SpringBootOnKubernetesTest {
 
@@ -33,7 +31,7 @@ class SpringBootOnKubernetesTest {
   public void shouldContainDeployment() {
     KubernetesList list = Serialization.unmarshal(SpringBootOnKubernetesTest.class.getClassLoader().getResourceAsStream("META-INF/ap4k/kubernetes.yml"));
     assertNotNull(list);
-    Deployment d = findFirst(list, Deployment.class).orElseThrow(()-> new IllegalStateException());
+    Deployment d = findFirst(list, Deployment.class).orElseThrow(() -> new IllegalStateException());
     assertNotNull(d);
   }
 
