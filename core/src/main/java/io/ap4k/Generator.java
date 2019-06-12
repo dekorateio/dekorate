f107dd44043c66cd599b897e82499b5ded694e7a
Chris Laprun <metacosm@gmail.com>
2019-06-12 17:22:58 +0200

    feat: fail if we encounter an unknown generator in properties

--- core/src/main/java/io/ap4k/Generator.java
+++ core/src/main/java/io/ap4k/Generator.java
@@ -1,31 +1,24 @@
 /**
  * Copyright 2018 The original authors.
- *
+ * <p>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *
-**/
+ **/
 package io.ap4k;
 
-import java.lang.annotation.Annotation;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Map;
-import java.util.ServiceLoader;
-
 import io.ap4k.utils.Generators;
+
+import java.lang.annotation.Annotation;
+import java.util.*;
 
 /**
  * Generates resources, based on the detected annotations.
@@ -44,9 +37,8 @@ public interface Generator extends SessionHandler {
     try {
       Thread.currentThread().setContextClassLoader(Generator.class.getClassLoader());
       ServiceLoader<Generator> loader = ServiceLoader.load(Generator.class);
-      Iterator<Generator> iterator = loader.iterator();
-      while (iterator.hasNext()) {
-        generators.add(iterator.next());
+      for (Generator generator : loader) {
+        generators.add(generator);
       }
     } finally {
       Thread.currentThread().setContextClassLoader(tccl);
@@ -59,16 +51,20 @@ public interface Generator extends SessionHandler {
    */
   static void init(Map<String, Object> map) {
     // Lazy load generators
-    List<Generator> generators = getGenerators();
+    getGenerators();
     for (Map.Entry<String, Object> entry : map.entrySet()) {
       String key = entry.getKey();
       Object value = entry.getValue();
       Generator generator = getGenerator(key);
-      if (value instanceof Map && generator != null) {
+      if (generator == null) {
+        throw new IllegalArgumentException("Unknown generator '" + key + "'. Known generators are: " + GENERATORS.keySet());
+      }
+
+      if (value instanceof Map) {
         Map<String, Object> generatorMap = new HashMap<>();
         Class annotationClass = ANNOTATIONS.get(key);
         String newKey = annotationClass.getName();
-        Generators.populateArrays(annotationClass, (Map<String, Object>)value);
+        Generators.populateArrays(annotationClass, (Map<String, Object>) value);
         generatorMap.put(newKey, value);
         generator.add(generatorMap);
       }
