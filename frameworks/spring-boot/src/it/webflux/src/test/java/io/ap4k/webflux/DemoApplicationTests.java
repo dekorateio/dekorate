df32bcc1b34181a6714042e396985785166d936c
Ioannis Canellos <iocanel@gmail.com>
2019-07-03 13:38:10 +0300

    feat: Add test inside webflux invoker test.

--- frameworks/spring-boot/src/it/webflux/src/test/java/io/ap4k/webflux/DemoApplicationTests.java
+++ frameworks/spring-boot/src/it/webflux/src/test/java/io/ap4k/webflux/DemoApplicationTests.java
@@ -1,16 +1,28 @@
 package io.ap4k.webflux;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import io.ap4k.utils.Serialization;
+import io.ap4k.deps.kubernetes.api.model.KubernetesList;
+import io.ap4k.deps.kubernetes.api.model.HasMetadata;
+import io.ap4k.deps.kubernetes.api.model.Service;
+import java.util.Optional;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class DemoApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+  @Test
+  public void shouldContainService() {
+    KubernetesList list = Serialization.unmarshal(getClass().getClassLoader().getResourceAsStream("META-INF/ap4k/kubernetes.yml"));
+    assertNotNull(list);
+    Service service = findFirst(list, Service.class).orElseThrow(IllegalStateException::new);
+    assertNotNull(service);
+    assertEquals(1, list.getItems().stream().filter(i -> Service.class.isInstance(i)).count());
+  }
 
+  <T extends HasMetadata> Optional<T> findFirst(KubernetesList list, Class<T> t) {
+    return (Optional<T>) list.getItems().stream()
+      .filter(i -> t.isInstance(i))
+      .findFirst();
+  }
 }
