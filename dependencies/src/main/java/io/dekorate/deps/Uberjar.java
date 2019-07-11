e34f263e7c1926202f803dbe1150e6c419b415e2
Ioannis Canellos <iocanel@gmail.com>
2019-07-11 11:36:28 +0300

    feat: Add dependencies uberjar as part of dekorate.

---
+++ dependencies/src/main/java/io/dekorate/deps/Uberjar.java
@@ -0,0 +1,25 @@
+/**
+ * Copyright (C) 2018 Red Hat inc.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+**/
+
+package io.dekorate.deps;
+
+/**
+ * This is just a shaded uberjar, with no real sources.
+ * This is a dummy class for adding to the sources and javadocs artifacts.
+ **/
+public class Uberjar {
+}
