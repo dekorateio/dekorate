fce0f85e1ca33674273ace0ea52aef3e5d730f70
Ioannis Canellos <iocanel@gmail.com>
2019-07-11 13:35:32 +0300

    feat: add integration test for capability without annotations.

---
+++ annotations/component-annotations/src/it/capability/src/main/java/io/dekorate/capability/DemoApplication.java
@@ -0,0 +1,13 @@
+package io.dekorate.capability;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
