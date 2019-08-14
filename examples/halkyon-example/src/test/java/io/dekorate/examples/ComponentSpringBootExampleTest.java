89323ee3e4d18734c7226afd9c64a3732440c3bf
Chris Laprun <metacosm@gmail.com>
2019-08-14 12:28:32 +0200

    fix: imports

--- examples/halkyon-example/src/test/java/io/dekorate/examples/ComponentSpringBootExampleTest.java
+++ examples/halkyon-example/src/test/java/io/dekorate/examples/ComponentSpringBootExampleTest.java
@@ -1,12 +1,12 @@
 /**
  * Copyright 2018 The original authors.
- *
+ * <p>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -21,25 +21,25 @@ import io.dekorate.deps.kubernetes.api.model.HasMetadata;
 import io.dekorate.deps.kubernetes.api.model.KubernetesList;
 import io.dekorate.halkyon.model.Component;
 import io.dekorate.utils.Serialization;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
 
 public class ComponentSpringBootExampleTest {
-
+  
   @Test
   public void shouldContainComponentWithDefaultValues() {
     KubernetesList list = Serialization.unmarshal(ComponentSpringBootExampleTest.class.getClassLoader().getResourceAsStream("META-INF/dekorate/halkyon.yml"));
     assertNotNull(list);
     List<HasMetadata> items = list.getItems();
-    Assertions.assertEquals(1, items.size());
+    assertEquals(1, items.size());
     Component component = (Component) items.get(0);
-    Assertions.assertEquals("Component", component.getKind());
+    assertEquals("Component", component.getKind());
     assertEquals("halkyon-example", component.getMetadata().getName());
     assertEquals(1, component.getMetadata().getLabels().size());
     assertNull(component.getSpec().getBuildConfig());
   }
-
+  
 }
