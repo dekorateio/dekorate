9fb2903e6277c7ff836b8c0e7b390a55f02fa540
aurea munoz <amunozhe@redhat.com>
2019-08-29 09:01:21 +0200

    test: add example of use to add a configMap to a volume with annotation
    Related to issue #306

---
+++ examples/kubernetes-example-with-configMapVolume/src/test/java/io/dekorate/examples/KubernetesExampleTest.java
@@ -0,0 +1,39 @@
+/**
+ * Copyright 2018 The original authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.dekorate.examples;
+
+import io.dekorate.utils.Serialization;
+import org.junit.jupiter.api.Test;
+import io.dekorate.deps.kubernetes.api.model.KubernetesList;
+import io.dekorate.deps.kubernetes.api.model.apps.Deployment;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+class KubernetesExampleTest {
+
+  @Test
+  public void shouldContainConfigMap() {
+    KubernetesList list = Serialization.unmarshal(KubernetesExampleTest.class.getClassLoader().getResourceAsStream("META-INF/dekorate/kubernetes.yml"));
+    assertNotNull(list);
+    assertEquals(1, list.getItems().size());
+    Deployment deployment = (Deployment) list.getItems().get(0);
+    assertEquals("Deployment", deployment.getKind());
+    assertEquals("bar-volume", deployment.getSpec().getTemplate().getSpec().getVolumes().get(0).getName());
+    assertEquals("foo-map", deployment.getSpec().getTemplate().getSpec().getVolumes().get(0).getConfigMap().getName());
+  }
+}
