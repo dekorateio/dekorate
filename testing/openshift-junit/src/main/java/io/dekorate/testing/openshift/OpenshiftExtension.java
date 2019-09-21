23dc691e07e38319943e2c6141635796594f8ab7
Ioannis Canellos <iocanel@gmail.com>
2019-09-21 15:30:07 +0300

    fix: Throw IllegalStateException when no BuildServiceFactory is applicable.

--- testing/openshift-junit/src/main/java/io/dekorate/testing/openshift/OpenshiftExtension.java
+++ testing/openshift-junit/src/main/java/io/dekorate/testing/openshift/OpenshiftExtension.java
@@ -106,7 +106,8 @@ public class OpenshiftExtension implements ExecutionCondition, BeforeAllCallback
 
     BuildService buildService = null;
     try {
-      BuildServiceFactory buildServiceFactory = BuildServiceFactories.find(getProject(), imageConfiguration).get();
+      BuildServiceFactory buildServiceFactory = BuildServiceFactories.find(getProject(), imageConfiguration)
+          .orElseThrow(() -> new IllegalStateException("No applicable BuildServiceFactory found."));
       buildService = buildServiceFactory.create(getProject(), imageConfiguration, list.getItems());
     } catch (Exception e) {
       throw DekorateException.launderThrowable("Failed to lookup BuildService.", e);
