3b6ba30cbcda481f3152a3e2faaaf5e80d557a3e
Ioannis Canellos <iocanel@gmail.com>
2019-10-07 19:57:50 +0300

    feat (#354): Add the ability to use annotationless style with generic java applications. Add vertx examples.

---
+++ core/src/main/java/io/dekorate/annotation/Dekorate.java
@@ -0,0 +1,44 @@
+/**
+ * Copyright 2018 The original authors.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+**/
+
+package io.dekorate.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import io.sundr.builder.annotations.Adapter;
+import io.sundr.builder.annotations.Buildable;
+import io.sundr.builder.annotations.Pojo;
+
+/**
+ * This annotations is meant to be used as a trigger of the dekorate process.
+ */
+
+@Buildable(builderPackage = "io.dekorate.deps.kubernetes.api.builder")
+@Pojo(name = "DekorateConifg", relativePath = "../config",
+      mutable = true,
+      withStaticBuilderMethod = false,
+      withStaticAdapterMethod = false,
+      adapter = @Adapter(suffix = "Adapter", relativePath = "../adapter", withMapAdapterMethod = true))
+@Target({ElementType.CONSTRUCTOR, ElementType.TYPE})
+@Retention(RetentionPolicy.SOURCE)
+public @interface Dekorate {
+
+  String[] resources() default {};
+}
