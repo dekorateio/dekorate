539a3d36a02c1a43cfd45b5d8b703e75ddbaff06
Ioannis Canellos <iocanel@gmail.com>
2019-10-07 20:54:13 +0300

    feat (#354): Add unit test for vertx examples.

---
+++ examples/vertx-on-openshift-example/src/test/java/io/dekorate/example/VertxTest.java
@@ -0,0 +1,39 @@
+/**
+ * Copyright 2018 The original authors.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+**/
+
+package io.dekorate.example;
+
+import java.util.Optional;
+import org.junit.jupiter.api.Test;
+
+import io.dekorate.deps.kubernetes.api.model.KubernetesList;
+import io.dekorate.deps.kubernetes.api.model.HasMetadata;
+import io.dekorate.deps.kubernetes.api.model.Service;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static io.dekorate.testing.KubernetesResources.*;
+
+class VertxTest {
+
+  @Test
+  public void shouldContainService() throws Exception {
+    KubernetesList list = loadGenerated("openshift");
+    Optional<Service> service = findFirst(list, Service.class);
+    assertTrue(service.isPresent());
+  }
+}
+
---
+++ testing/core-junit/src/main/java/io/dekorate/testing/KubernetesResources.java
@@ -0,0 +1,40 @@
+/**
+ * Copyright 2018 The original authors.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+**/
+
+package io.dekorate.testing;
+
+import java.util.Optional;
+
+import io.dekorate.deps.kubernetes.api.model.HasMetadata;
+import io.dekorate.deps.kubernetes.api.model.KubernetesList;
+import io.dekorate.utils.Serialization;
+
+public class KubernetesResources {
+
+  public static KubernetesList loadGenerated(String group) {
+    return Serialization.unmarshal(Serialization.class.getClassLoader().getResourceAsStream("META-INF/dekorate/" + group + ".yml"));
+  }
+  /**
+   * Find the first resource of the specified type.
+   * @param list The list with the generated resources.
+   * @param t The type of resource that we are looking for.
+   * @return An optional containing the resource.
+   */
+  public static <T extends HasMetadata> Optional<T> findFirst(KubernetesList list, Class<T> t) {
+    return (Optional<T>) list.getItems().stream().filter(i -> t.isInstance(i)).findFirst();
+  }
+}
