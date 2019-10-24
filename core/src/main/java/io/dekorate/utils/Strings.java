5b685c2a1665b97f33f295cfde7eeeb3eb7982b5
Ioannis Canellos <iocanel@gmail.com>
2019-10-24 20:31:09 +0300

    feat: Jib build service now also works with gradle.

--- core/src/main/java/io/dekorate/utils/Strings.java
+++ core/src/main/java/io/dekorate/utils/Strings.java
@@ -15,11 +15,13 @@
  */
 package io.dekorate.utils;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Arrays;
-import java.util.Iterator;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.stream.Collectors;
 
@@ -126,6 +128,14 @@ public class Strings {
   public static String read(Path path) {
     try {
       return new String(Files.readAllBytes(path));
+    } catch (IOException e) {
+      throw DekorateException.launderThrowable(e);
+    }
+  }
+
+ public static String read(InputStream is) {
+    try (BufferedReader buffer = new BufferedReader(new InputStreamReader(is))) {
+      return buffer.lines().collect(Collectors.joining("\n"));
     } catch (IOException e) {
       throw DekorateException.launderThrowable(e);
     }
