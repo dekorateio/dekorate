ac3af5a7d5416e1808f0d9222374c9f5f70c7e24
Ioannis Canellos <iocanel@gmail.com>
2019-12-04 22:42:27 +0200

    test (#420): Add test case to test setting the docker registry.

---
+++ tests/issue-420-docker-registry/src/test/java/io/dekorate/examples/kuberentes/Issue420Test.java
@@ -0,0 +1,52 @@
+/**
+ * Copyright 2018 The original authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.dekorate.examples.kubernetes;
+
+import io.dekorate.deps.kubernetes.api.model.HasMetadata;
+import io.dekorate.deps.kubernetes.api.model.Container;
+import io.dekorate.deps.kubernetes.api.model.Probe;
+import io.dekorate.deps.kubernetes.api.model.KubernetesList;
+import io.dekorate.deps.kubernetes.api.model.apps.Deployment;
+import io.dekorate.utils.Serialization;
+import java.net.URL;
+import java.util.Optional;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class Issue420Test {
+
+  @Test
+  public void shouldHaveRegistry() {
+    KubernetesList list = Serialization.unmarshalAsList(Issue420Test.class.getClassLoader().getResourceAsStream("META-INF/dekorate/kubernetes.yml"));
+    assertNotNull(list);
+    Deployment d = findFirst(list, Deployment.class).orElseThrow(() -> new IllegalStateException());
+    assertNotNull(d);
+    Container c = d.getSpec().getTemplate().getSpec().getContainers().get(0);
+    assertNotNull(c);
+    String image = c.getImage();
+    assertTrue(image.startsWith("localhost:3000"));
+  }
+
+  <T extends HasMetadata> Optional<T> findFirst(KubernetesList list, Class<T> t) {
+    return (Optional<T>) list.getItems().stream()
+      .filter(i -> t.isInstance(i))
+      .findFirst();
+  }
+
+}
