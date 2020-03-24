8b8b821f3ab2e12e51f1e695401b898189d592da
Chris Laprun <metacosm@gmail.com>
2020-03-24 16:05:47 +0100

    fix: RequiredComponentCapability inherits from ComponentCapability and has parameters

--- annotations/halkyon-annotations/src/main/java/io/dekorate/halkyon/model/RequiredComponentCapability.java
+++ annotations/halkyon-annotations/src/main/java/io/dekorate/halkyon/model/RequiredComponentCapability.java
@@ -1,24 +1,21 @@
 package io.dekorate.halkyon.model;
 
+import io.dekorate.deps.kubernetes.api.model.Doneable;
 import io.sundr.builder.annotations.Buildable;
 import io.sundr.builder.annotations.Inline;
-import io.dekorate.deps.kubernetes.api.model.Doneable;
 
 @Buildable(editableEnabled = false, builderPackage = "io.dekorate.deps.kubernetes.api.builder", inline = @Inline(type = Doneable.class, prefix = "Doneable", value = "done"))
-public class RequiredComponentCapability {
-
-  private String name;
+public class RequiredComponentCapability extends ComponentCapability {
   private String boundTo;
   private boolean autoBindable;
-  private CapabilitySpec spec;
 
   public RequiredComponentCapability() {
   }
 
-  public RequiredComponentCapability(String boundTo, boolean autoBindable, String name) {
+  public RequiredComponentCapability(String name, CapabilitySpec spec, Parameter[] parameters, String boundTo, boolean autoBindable) {
+    super(name, spec, parameters);
     this.boundTo = boundTo;
     this.autoBindable = autoBindable;
-    this.name = name;
   }
 
   public String getBoundTo() {
@@ -27,22 +24,6 @@ public class RequiredComponentCapability {
 
   public void setBoundTo(String boundTo) {
     this.boundTo = boundTo;
-  }
-
-  public String getName() {
-    return name;
-  }
-
-  public void setName(String name) {
-    this.name = name;
-  }
-
-  public CapabilitySpec getSpec() {
-    return spec;
-  }
-
-  public void setSpec(CapabilitySpec spec) {
-    this.spec = spec;
   }
 
   public boolean isAutoBindable() {
