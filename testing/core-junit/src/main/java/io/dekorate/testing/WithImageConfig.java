d6c05d754da08ccbebc2c8f9d541898719886ef9
Ioannis Canellos <iocanel@gmail.com>
2020-05-07 18:06:35 +0300

    feat (#392): Allow generating manifests outside target.

--- testing/core-junit/src/main/java/io/dekorate/testing/WithImageConfig.java
+++ testing/core-junit/src/main/java/io/dekorate/testing/WithImageConfig.java
@@ -1,42 +1,46 @@
 /**
  * Copyright 2018 The original authors.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- * 
+ *
 **/
 
 package io.dekorate.testing;
 
-import java.net.URL;
+import java.nio.file.Path;
 import java.util.Optional;
 import java.util.stream.Stream;
 
 import io.dekorate.BuildServiceFactories;
 import io.dekorate.kubernetes.config.ImageConfiguration;
+import io.dekorate.project.Project;
 import io.dekorate.utils.Serialization;
 
 public interface WithImageConfig extends WithProject {
 
-  String DEKORATE_ROOT = "META-INF/dekorate";
-  String CONFIG_PATH = DEKORATE_ROOT + "/.config/%s.yml";
+  String CONFIG_YML = "%s.yml";
+  String CONFIG_DIR = "config";
 
   default <C extends ImageConfiguration> Stream<C> stream(Class<C> type) {
+    final Project project = getProject();
+    final Path configDir = project.getBuildInfo().getClassOutputDir().resolve(project.getDekorateMetaDir()).resolve(CONFIG_DIR);
+
     return BuildServiceFactories.names()
       .stream()
-      .map(n -> String.format(CONFIG_PATH, n))
-      .map(r -> WithImageConfig.class.getClassLoader().getResource(r))
-      .filter(u -> u != null)
-      .map(u -> Serialization.unmarshal(u, ImageConfiguration.class))
+      .map(n -> String.format(CONFIG_YML, n))
+      .map(s -> configDir.resolve(s))
+      .filter(p -> p.toFile().exists())
+      .map(p -> Serialization.unmarshal(p.toFile(), ImageConfiguration.class))
       .filter(BuildServiceFactories.configMatches(getProject()))
       .filter(i -> type.isInstance(i))
       .map(i -> (C) i);
@@ -49,4 +53,5 @@ public interface WithImageConfig extends WithProject {
   default Optional<ImageConfiguration> getImageConfig() {
     return stream(ImageConfiguration.class).findFirst();
   }
+
 }
