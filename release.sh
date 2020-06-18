fb34572b4a4c88089f2456312d510c4fd2d04790
Ioannis Canellos <iocanel@gmail.com>
2020-06-18 12:46:49 +0300

    doc(#538) Automatically bump release version in docs

--- release.sh
+++ release.sh
@@ -5,6 +5,18 @@
 # curl -s https://raw.githubusercontent.com/shellib/grab/master/install.sh | bash
 #
 
+source $(grab github.com/shellib/cli)
 source $(grab github.com/shellib/maven as maven)
 
+release_version=$(readopt --release-version $*)
+if [ -z $release_version ]; then
+  echo "Option --release_version is required!"
+  exit 1
+fi
+
+# Update the docs with the releae version
+./scripts/ChangeVersion.java readme.md io.dekorate $release_version > readme.md.versionChanged
+mv readme.md.versionChanged reamde.md
+git add readme.md
+git commit -m "doc: Update dekorate version in docs to $release_version"
 maven::release $*
