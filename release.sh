76d4fcefc57f4b0390f2844bbf633129e252362f
Ioannis Canellos <iocanel@gmail.com>
2020-07-07 09:41:03 +0300

    chore (#579): Set the examples version before release

--- release.sh
+++ release.sh
@@ -19,4 +19,9 @@ fi
 mv readme.md.versionChanged readme.md
 git add readme.md
 git commit -m "doc: Update dekorate version in docs to $release_version"
+mvn versions:set -DnewVersion=$release_version -Pwith-examples
+git add examples
+git commit -m "chore: Set examples version to $release_version"
+git stash save --keep-index --include-untracked
+git stash drop
 maven::release $*
