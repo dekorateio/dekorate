c234f6720bf7851ee01b46a6cd1e9f2941bab438
Ioannis Canellos <iocanel@gmail.com>
2020-09-10 17:52:36 +0300

    chore: Remove uneeded classes.

--- core/src/main/java/io/dekorate/kubernetes/decorator/ApplyImagePullPolicyDecorator.java
+++ core/src/main/java/io/dekorate/kubernetes/decorator/ApplyImagePullPolicyDecorator.java
@@ -32,7 +32,7 @@ public class ApplyImagePullPolicyDecorator extends Decorator<ContainerFluent>  {
   }
 
   public Class<? extends Decorator>[] after() {
-    return new Class[]{ResourceProvidingDecorator.class, ContainerDecorator.class, AddSidecarDecorator.class};
+    return new Class[]{ResourceProvidingDecorator.class, AddSidecarDecorator.class};
   }
 
 }
