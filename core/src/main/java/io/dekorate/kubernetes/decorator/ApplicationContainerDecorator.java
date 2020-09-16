ae526e9206b637bb210530cbebbc60ed007bb5fd
Ioannis Canellos <iocanel@gmail.com>
2020-09-16 16:32:52 +0300

    feat (#623) ApplicationContainerDecorator can be used on metadataless objects.

--- core/src/main/java/io/dekorate/kubernetes/decorator/ApplicationContainerDecorator.java
+++ core/src/main/java/io/dekorate/kubernetes/decorator/ApplicationContainerDecorator.java
@@ -62,7 +62,7 @@ public abstract class ApplicationContainerDecorator<T> extends Decorator<Visitab
   @Override
   public void visit(VisitableBuilder builder) {
     Optional<ObjectMeta> objectMeta = getMetadata(builder);
-    if (!objectMeta.isPresent()) {
+    if (Strings.isNotNullOrEmpty(deploymentName) && !objectMeta.isPresent()) {
       return;
     }
     if (Strings.isNullOrEmpty(deploymentName) || objectMeta.map(m -> m.getName()).filter(s -> s.equals(deploymentName)).isPresent()) {
