f47ce7004fa26ed6681a05c604d5d827adba0969
Ioannis Canellos <iocanel@gmail.com>
2020-10-20 05:42:13 +0300

    feat (#659) Implement UseLocalDockerConfigJsonConfigurator

---
+++ annotations/tekton-annotations/src/main/java/io/dekorate/tekton/configurator/UseLocaDockerConfigJsonConfigurator.java
@@ -0,0 +1,19 @@
+
+package io.dekorate.tekton.configurator;
+
+import io.dekorate.kubernetes.config.Configurator;
+import io.dekorate.tekton.config.TektonConfigFluent;
+
+public class UseLocaDockerConfigJsonConfigurator extends Configurator<TektonConfigFluent<?>> {
+
+  private final boolean enabled;
+  
+	public UseLocaDockerConfigJsonConfigurator(boolean enabled) {
+		this.enabled = enabled;
+	}
+
+	@Override
+	public void visit(TektonConfigFluent<?> config) {
+    config.withUseLocalDockerConfigJson(enabled);
+	}
+}
