a1f9cb29f81de2698ce3e4b25ea20fa8b4a4f0ea
Ioannis Canellos <iocanel@gmail.com>
2020-12-16 12:53:07 +0200

    fix: Add knative specific decorators for volumes.

---
+++ annotations/knative-annotations/src/main/java/io/dekorate/knative/decorator/AddAwsElasticBlockStoreVolumeToRevisionDecorator.java
@@ -0,0 +1,52 @@
+/**
+ * Copyright 2018 The original authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.dekorate.knative.decorator;
+
+import io.dekorate.doc.Description;
+import io.dekorate.kubernetes.config.AwsElasticBlockStoreVolume;
+import io.dekorate.kubernetes.decorator.NamedResourceDecorator;
+import io.fabric8.knative.serving.v1.RevisionSpecFluent;
+import io.fabric8.kubernetes.api.model.ObjectMeta;
+
+@Description("Add an elastic block store volume to the pod spec.")
+public class AddAwsElasticBlockStoreVolumeToRevisionDecorator extends NamedResourceDecorator<RevisionSpecFluent<?>> {
+
+  private final AwsElasticBlockStoreVolume volume;
+
+  public AddAwsElasticBlockStoreVolumeToRevisionDecorator(AwsElasticBlockStoreVolume volume) {
+    this(ANY, volume);
+  }
+
+  public AddAwsElasticBlockStoreVolumeToRevisionDecorator(String name, AwsElasticBlockStoreVolume volume) {
+    super(name);
+    this.volume = volume;
+  }
+
+  @Override
+  public void andThenVisit(RevisionSpecFluent<?> revisionSpec, ObjectMeta resourceMeta) {
+    revisionSpec.addNewVolume()
+        .withName(volume.getVolumeName())
+        .withNewAwsElasticBlockStore()
+        .withVolumeID(volume.getVolumeId())
+        .withFsType(volume.getFsType())
+        .withPartition(volume.getPartition())
+        .withReadOnly(volume.isReadOnly())
+        .endAwsElasticBlockStore()
+        .endVolume();
+  }
+
+	
+}
