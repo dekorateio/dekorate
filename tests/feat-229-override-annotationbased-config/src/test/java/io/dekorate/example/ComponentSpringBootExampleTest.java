2f0af70a33ab894b4da836d848a875e5996456b7
Ioannis Canellos <iocanel@gmail.com>
2021-01-13 13:21:31 +0200

    refactor: align test package naming

---
+++ tests/feat-229-override-annotationbased-config/src/test/java/io/dekorate/example/ComponentSpringBootExampleTest.java
@@ -0,0 +1,57 @@
+/**
+ * Copyright 2018 The original authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.dekorate.example;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import java.util.List;
+import java.util.Map;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import io.dekorate.halkyon.model.Component;
+import io.dekorate.halkyon.model.DeploymentMode;
+import io.dekorate.utils.Serialization;
+import io.fabric8.kubernetes.api.model.HasMetadata;
+import io.fabric8.kubernetes.api.model.KubernetesList;
+
+public class ComponentSpringBootExampleTest {
+
+  @Test
+  public void shouldContainComponent() {
+    KubernetesList list = Serialization.unmarshalAsList(
+        ComponentSpringBootExampleTest.class.getClassLoader().getResourceAsStream("META-INF/dekorate/halkyon.yml"));
+    assertNotNull(list);
+    List<HasMetadata> items = list.getItems();
+    Assertions.assertEquals(1, items.size());
+    Component component = (Component) items.get(0);
+    Assertions.assertEquals("Component", component.getKind());
+    // This doesn't work during release.
+    //assertEquals("https://github.com/dekorateio/dekorate.git", component.getSpec().getBuildConfig().getUrl());
+    assertEquals("docker", component.getSpec().getBuildConfig().getType());
+    assertEquals("feat-229-override-annotationbased-config", component.getSpec().getBuildConfig().getModuleDirName());
+    // This may be null during the release process where HEAD point to a commit instead of a branch.
+    //assertNotNull("", component.getSpec().getBuildConfig().getRef());
+    assertEquals(DeploymentMode.build, component.getSpec().getDeploymentMode());
+    Map<String, String> labels = component.getMetadata().getLabels();
+    Assertions.assertNotNull(labels.get("key1-from-properties"));
+    Assertions.assertEquals("val1-from-properties", labels.get("key1-from-properties"));
+    Assertions.assertEquals("hello-world", component.getMetadata().getName());
+  }
+
+}
---
+++ tests/issue-420-docker-registry/src/test/java/io/dekorate/example/Issue420Test.java
@@ -0,0 +1,53 @@
+/**
+ * Copyright 2018 The original authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.dekorate.example;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.Optional;
+
+import org.junit.jupiter.api.Test;
+
+import io.dekorate.utils.Serialization;
+import io.fabric8.kubernetes.api.model.Container;
+import io.fabric8.kubernetes.api.model.HasMetadata;
+import io.fabric8.kubernetes.api.model.KubernetesList;
+import io.fabric8.kubernetes.api.model.apps.Deployment;
+
+public class Issue420Test {
+
+  @Test
+  public void shouldHaveRegistry() {
+    KubernetesList list = Serialization
+        .unmarshalAsList(Issue420Test.class.getClassLoader().getResourceAsStream("META-INF/dekorate/kubernetes.yml"));
+    assertNotNull(list);
+    Deployment d = findFirst(list, Deployment.class).orElseThrow(() -> new IllegalStateException());
+    assertNotNull(d);
+    Container c = d.getSpec().getTemplate().getSpec().getContainers().get(0);
+    assertNotNull(c);
+    String image = c.getImage();
+    assertTrue(image.startsWith("localhost:3000"));
+  }
+
+  <T extends HasMetadata> Optional<T> findFirst(KubernetesList list, Class<T> t) {
+    return (Optional<T>) list.getItems().stream()
+        .filter(i -> t.isInstance(i))
+        .findFirst();
+  }
+
+}
