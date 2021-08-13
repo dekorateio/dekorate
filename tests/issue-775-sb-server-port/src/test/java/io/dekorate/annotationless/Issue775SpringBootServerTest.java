c586263d9126418fec431cac2decbb4a64f23c48
Charles Moulliard <ch007m@gmail.com>
2021-08-13 14:24:24 +0200

    test: port mapping on spring boot apps with single port

---
+++ tests/issue-775-sb-server-port/src/test/java/io/dekorate/annotationless/Issue775SpringBootServerTest.java
@@ -0,0 +1,58 @@
+/**
+ * Copyright 2018 The original authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.dekorate.annotationless;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.junit.jupiter.api.Test;
+
+import io.dekorate.utils.Serialization;
+import io.fabric8.kubernetes.api.model.HasMetadata;
+import io.fabric8.kubernetes.api.model.KubernetesList;
+import io.fabric8.kubernetes.api.model.Service;
+import io.fabric8.kubernetes.api.model.ServicePort;
+
+public class Issue775SpringBootServerTest {
+
+  @Test
+  public void shouldHaveSpringBootServerPort() {
+    KubernetesList list = Serialization
+        .unmarshalAsList(getClass().getClassLoader().getResourceAsStream("META-INF/dekorate/kubernetes.yml"));
+    assertNotNull(list);
+    Service s = findFirst(list, Service.class).orElseThrow(() -> new IllegalStateException());
+    assertNotNull(s);
+
+    List<ServicePort> p = s.getSpec().getPorts();
+    assertEquals(1, p.size());
+
+    ServicePort servicePort1 = p.get(0);
+    assertEquals("http", servicePort1.getName());
+    assertEquals(8090, servicePort1.getPort());
+    assertEquals(8090, servicePort1.getTargetPort().getIntVal());
+  }
+
+  <T extends HasMetadata> Optional<T> findFirst(KubernetesList list, Class<T> t) {
+    return (Optional<T>) list.getItems().stream()
+        .filter(i -> t.isInstance(i))
+        .findFirst();
+  }
+
+}
