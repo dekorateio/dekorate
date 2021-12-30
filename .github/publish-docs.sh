7ce4b689311a46ff388e0fc75447ff187f9689c0
Auri Munoz <amunozhe@redhat.com>
2021-12-30 19:11:49 +0100

    doc: add some details about how the doc is generated
    Fixes #836

--- .github/publish-docs.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-version=$1;
-
-yq eval ".release.current-version = \"$version\"" docs/_data/project.yml | sponge docs/_data/project.yml
-git checkout -b jekyll-site
-git add docs/_data/project.yml
-git commit -m "chore: update project version for Dekorate docs."
-git push origin -u jekyll-site
-
