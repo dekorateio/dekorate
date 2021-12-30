24d433b52e72f178dee296186ecd5dccd4ac096a
Auri Munoz <amunozhe@redhat.com>
2021-12-30 16:13:27 +0100

    commit project file with version

--- .github/publish-docs.sh
+++ .github/publish-docs.sh
@@ -3,4 +3,7 @@ version=$1;
 echo "--- estoy aqui: $pwd"
 ls -lisa
 yq eval ".release.current-version = \"$version\"" docs/_data/project.yml | sponge docs/_data/project.yml
+git add docs/_data/project.yml
+git commit -m "chore: updata project version for Dekorate docs."
+git push auri -u jekyll-site
 
