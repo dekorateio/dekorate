2476874e9703fddbc86a8faf5e03e9465755a63d
Auri Munoz <amunozhe@redhat.com>
2021-12-30 16:58:42 +0100

    change email and fix typo

--- .github/publish-docs.sh
+++ .github/publish-docs.sh
@@ -4,6 +4,6 @@ version=$1;
 yq eval ".release.current-version = \"$version\"" docs/_data/project.yml | sponge docs/_data/project.yml
 git checkout -b jekyll-site
 git add docs/_data/project.yml
-git commit -m "chore: updata project version for Dekorate docs."
+git commit -m "chore: update project version for Dekorate docs."
 git push origin -u jekyll-site
 
