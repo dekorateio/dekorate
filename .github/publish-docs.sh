ddf6a41f0180e114c590d48e4a9eb46d89dac4a6
Auri Munoz <amunozhe@redhat.com>
2021-12-30 16:08:27 +0100

    fix path

--- .github/publish-docs.sh
+++ .github/publish-docs.sh
@@ -2,5 +2,5 @@
 version=$1;
 echo "--- estoy aqui: $pwd"
 ls -lisa
-yq eval ".release.current-version = \"$version\"" ../docs/_data/project.yml | sponge ../docs/_data/project.yml
+yq eval ".release.current-version = \"$version\"" docs/_data/project.yml | sponge docs/_data/project.yml
 
