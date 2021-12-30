cd7864266c032c5258df74c5c8e48c75e2a17c42
Auri Munoz <amunozhe@redhat.com>
2021-12-30 16:39:53 +0100

    add checkout to avoid detached head

--- .github/publish-docs.sh
+++ .github/publish-docs.sh
@@ -2,7 +2,7 @@
 version=$1;
 
 yq eval ".release.current-version = \"$version\"" docs/_data/project.yml | sponge docs/_data/project.yml
-git checkout jekyll-site
+git checkout -b jekyll-site
 git add docs/_data/project.yml
 git commit -m "chore: updata project version for Dekorate docs."
 git push origin -u jekyll-site
