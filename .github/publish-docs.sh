45b0a733784f231fb59c9a65e177ad8039978f58
Auri Munoz <amunozhe@redhat.com>
2021-12-30 16:17:29 +0100

    fix git identity error

--- .github/publish-docs.sh
+++ .github/publish-docs.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 version=$1;
-echo "--- estoy aqui: $pwd"
-ls -lisa
+
 yq eval ".release.current-version = \"$version\"" docs/_data/project.yml | sponge docs/_data/project.yml
 git add docs/_data/project.yml
 git commit -m "chore: updata project version for Dekorate docs."
