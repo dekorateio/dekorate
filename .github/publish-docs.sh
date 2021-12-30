8866b5ccc32e8c914cf16e875d77d6f6ccb8a18c
Auri Munoz <amunozhe@redhat.com>
2021-12-30 16:29:25 +0100

    fix git identity error

--- .github/publish-docs.sh
+++ .github/publish-docs.sh
@@ -4,5 +4,5 @@ version=$1;
 yq eval ".release.current-version = \"$version\"" docs/_data/project.yml | sponge docs/_data/project.yml
 git add docs/_data/project.yml
 git commit -m "chore: updata project version for Dekorate docs."
-git push auri -u jekyll-site
+git push origin -u jekyll-site
 
