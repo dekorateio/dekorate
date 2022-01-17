a1368cb152d5d0754097a2166eaafc7a7540cee3
James Cobb <jcobb@redhat.com>
2022-01-17 13:23:29 -0600

    doc page adjustments. Add on page pulldown navigation button for mobile and adjust content column widths to match wrapper.

---
+++ docs/assets/javascript/nav_mobilesubnav.js
@@ -0,0 +1,8 @@
+function myFunction1() {
+  var x = document.getElementById("sidebartoggle");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
