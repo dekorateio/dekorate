ec38d002474afce3f3841a0de7682d5131eb8cf7
Ioannis Canellos <iocanel@gmail.com>
2024-11-26 12:36:03 +0200

    fix: wrong package for Main class

--- tests/feat-kubernetes-nodeselector-annotation/src/main/java/io/dekorate/example/Main.java
+++ tests/feat-kubernetes-nodeselector-annotation/src/main/java/io/dekorate/example/Main.java
@@ -24,6 +24,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Main {
   public static void main(String[] args) {
-    SpringApplication.run(com.sun.tools.javac.Main.class, args);
+    SpringApplication.run(Main.class, args);
   }
 }
